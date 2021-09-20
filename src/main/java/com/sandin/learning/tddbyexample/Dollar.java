c775847a8e55f5b5ed43d97254189b7e8cd7f421
Sandin <sandina@emea.roche.com>
2021-09-20 15:53:15 +0200

    first test running Multi-Currency

---
+++ src/main/java/com/sandin/learning/tddbyexample/Dollar.java
@@ -0,0 +1,13 @@
+package com.sandin.learning.tddbyexample;
+
+public class Dollar {
+
+    int amount = 10;
+
+    public Dollar(int amount) {
+    }
+
+    public void times(int multiplier) {
+
+    }
+}
--- src/test/java/com/sandin/learning/tddbyexample/TddByExampleApplicationTests.java
+++ src/test/java/com/sandin/learning/tddbyexample/TddByExampleApplicationTests.java
@@ -3,6 +3,8 @@ package com.sandin.learning.tddbyexample;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class TddByExampleApplicationTests {
 
@@ -10,4 +12,10 @@ class TddByExampleApplicationTests {
 	void contextLoads() {
 	}
 
+	@Test
+	public void testMultiplication() {
+		Dollar five = new Dollar(5);
+		five.times(2);
+		assertEquals(10, five.amount);
+	}
 }
